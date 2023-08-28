7164255134bab444c69b10725b0b11dfe5968903
Sanket8805 <pawarsanket997@gmail.com>
2023-08-28 15:11:30 -0400

    added code for news app

---
+++ p2_newsproject/newsapp/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render,redirect
+from requests import *
+
+def pnf(request, exception):
+	return redirect("home")
+
+def about(request):
+	return render(request,"about.html")
+
+	
+def home(request):
+	try:
+		a1= "https://newsapi.org/v2/top-headlines"
+		a2= "?country=in"
+		a3= "&apiKey=d99e2845c4624739b8e4c1f6a6c2872e"
+		wa = a1 + a2 + a3
+		res=get(wa)
+		data=res.json()
+		info = data["articles"]
+		msg = info
+		return render(request,"home.html",{"msg":msg})
+	except Exception as e:
+		return render(request,"home.html",{"msg":str(e)})
+
